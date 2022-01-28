bd8864189183e2e2c9b5a24ce9a6b9f32a02f5af
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-28 09:53:29 +0000

    serializer token config

--- users/tokens.py
+++ users/tokens.py
@@ -74,7 +74,22 @@ class Token:
             at_time = self.current_time
             
         self.payload[claim] = datetime_to_epoch(at_time)
-    
+
+    def __repr__(self):
+        return repr(self.payload)
+
+    def __getitem__(self, key):
+        return self.payload[key]
+
+    def __setitem__(self, key, value):
+        self.payload[key] = value
+
+    def __delitem__(self, key):
+        del self.payload[key]
+
+    def __contains__(self, key):
+        return key in self.payload
+     
     def get(self, key, default=None):
         return self.payload.get(key, default)
     
